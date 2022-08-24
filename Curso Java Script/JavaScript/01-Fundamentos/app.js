934c647e780759b13b9f5d057252e8a1bb22860c
Luis muñoz <106047014+luisMoreno070@users.noreply.github.com>
2022-08-24 18:35:55 -0500

    Add files via upload

---
+++ Curso Java Script/JavaScript/01-Fundamentos/app.js
@@ -0,0 +1,17 @@
+// console.log('Hola Mundo');
+
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a + b;
+
+const saludo = c + d ;
+
+     
+//console.log( x );  
+//console.log('%c Mis Variables', 'color:blue; font-weight: bold');
+//console.log({a});
+//console.log({b});
+//console.log({b});
+//console.table({a, b, c, d, x,});
